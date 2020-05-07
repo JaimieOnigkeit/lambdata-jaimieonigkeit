bc35474325f6ab74a5294693078fdb4ffaa788fb
jaimieonigkeit <jaimie.onigkeit@gmail.com>
2020-05-06 21:03:20 -0600

    Prepare to publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="my-lambdata-jaimieonigkeit", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Jaimie Onigkeit",
     author_email="jaimie.onigkeit@gmail.com",
     description="A short description",
